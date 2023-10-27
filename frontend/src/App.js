e80af9a136f79add0b7315f9ccea251369d55050
rstoyanovv <rostistoqnov@gmail.com>
2023-10-27 17:44:53 +0300

    Start writing components and context files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { WelcomePage } from './components/WelcomePage';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={< WelcomePage />}></Route>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ frontend/src/context/ArticleContext.js
@@ -0,0 +1,25 @@
+import { createContext, useContext, useEffect } from "react";
+import axios from "axios";
+
+const ArticleContext = createContext();
+
+export const ArticleProvider = () => {
+    const [articles, setArticles] = useContext([]);
+
+    useEffect(() => {
+        const fetchArticles = () => {
+            try {
+                const result = axios.get(`/api/articles`);
+                const articles = result.data;
+                setArticles(articles);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        fetchArticles();
+    }, []);
+
+    //Console test
+    console.log(articles);
+
+}
