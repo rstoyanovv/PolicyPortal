3a2be8f6e92bddb83cecf32e52b43bae47259343
rstoyanovv <rostistoqnov@gmail.com>
2023-12-10 22:01:36 +0200

    Update

--- frontend/src/innerRouters/LoginAndRegistrationInnerRouter.js
+++ frontend/src/innerRouters/LoginAndRegistrationInnerRouter.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import InformativeLoginScreen from "../screens/LoginAndRegistration/InformativeLoginScreen";
 import RegistrationScreen from "../screens/LoginAndRegistration/RegistationScreen";
+import LoginScreen from "../screens/LoginAndRegistration/LoginScreen";
 
 export default function LoginAndRegistrationInnerRouter() {
     return (
         <Routes>
             <Route path="/" element={<InformativeLoginScreen />} />
             <Route path="/register" element={<RegistrationScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
         </Routes>
     );
 }
